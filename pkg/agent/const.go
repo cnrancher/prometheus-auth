316b57842dab8b04587eb8859383d69d6c3f726b
michelia feng <michelia.feng@gmail.com>
2020-07-02 18:09:34 +0800

    PANDARIA: update code base on latest vendor

---
+++ pkg/agent/const.go
@@ -0,0 +1,9 @@
+package agent
+
+const (
+	contentTypeHeader     = "Content-Type"
+	contentEncodingHeader = "Content-Encoding"
+	acceptHeader          = "Accept"
+	jsonContentType       = "application/json"
+	protoContentType      = "application/x-protobuf"
+)
